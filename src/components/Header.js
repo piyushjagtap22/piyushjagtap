1b478aa52cdb8f03eb7d9331822fae1ae360d576
piyushjagtap22 <jagspiyush@gmail.com>
2023-06-12 18:01:12 +0530

    Portfolio

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import NavLinks from "./NavLinks";
+
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img className="logo"  />
+      <NavLinks />
+    </header>
+  );
+};
+
+export default Header;
